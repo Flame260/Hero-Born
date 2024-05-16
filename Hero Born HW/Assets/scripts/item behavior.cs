276bf677cf95a1187f0970a7e1042844d87dcb2d
Flame260 <162753205+Flame260@users.noreply.github.com>
2024-05-16 10:26:56 -0400

    item collected

--- Hero Born HW/Assets/scripts/item behavior.cs
+++ Hero Born HW/Assets/scripts/item behavior.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class itembehavior : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void OnCollisionEnter(Collision collision)
     {
-        
+        if(collision.gameObject.name == "player")
+        {
+            Destroy(this.transform.gameObject);
+//after picking up items
+            Debug.Log(" item collected! ");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
